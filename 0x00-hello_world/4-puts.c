831ec2c2ae4716c450594a14a18c56144a18f2fe
jifel <jiffaelorm@gmail.com>
2022-09-08 20:30:00 -0700

    priting with puts function an dbetty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,7 +7,7 @@ include <stdio.h>
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
