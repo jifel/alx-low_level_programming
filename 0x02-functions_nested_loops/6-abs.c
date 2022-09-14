f9777bbcbc2757c70c8adb4f4624070c098ad936
jifel <jiffaelorm@gmail.com>
2022-09-14 08:16:54 -0700

    included parameters

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 /**
  * _abs - entry point
- * @int: accepts int value as parameter
+ * @a: accepts int value as parameter
  * Description: computes absolute value of an integer
  * Return: 0
  */
 int _abs(int a)
 {
-	int value;
+	int a, value;
 
 	value = abs(a);
 	_putchar(value);
