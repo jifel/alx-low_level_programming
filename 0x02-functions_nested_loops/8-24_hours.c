d4e4799385910620ea41e8556f0a89d1db5b292c
jifel <jiffaelorm@gmail.com>
2022-09-14 13:22:03 -0700

    remove wrong variable in the minutes for loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 
 	for (h = 0; h < 24; h++)
 	{
-		for (m = 0; h < 60; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar(h / 10 + '0');
 			_putchar(h % 10 + '0');
@@ -19,6 +19,7 @@ void jack_bauer(void)
 			_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');
 			_putchar('\n');
+
 		}
 	}
 }
