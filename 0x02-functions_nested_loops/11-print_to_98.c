cbc14a794521ca8a0bb69113dd63116101a617a8
jifel <jiffaelorm@gmail.com>
2022-09-14 22:52:24 -0700

    updates

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,30 +9,28 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+
+	if (i < 98)
 	{
-		if (i < 98)
+		while (i <= 98)
 		{
-			while (i <= 98)
-			{
 
-				printf("%d, ", i--);
-
-			}
-			printf("%d\n", i);
-
+			printf("%d, ", i--);
 
 		}
-		else
-		{
-			while (n < 98)
-			{
+		printf("%d\n", i);
 
-				printf("%d, ", i++);
-			}
-			printf("%d\n", i);
+
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", i++);
 
 		}
+		printf("%d\n", i);
+
 	}
 
 }
