7320689f6a8a6f206bcec2b4a94f6cb6f00ea2ab
jifel <jiffaelorm@gmail.com>
2022-09-19 15:21:30 -0700

    prints a tring to stdout followed by newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by newline to  stdout
+ * @str: the char parameter it points to
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
