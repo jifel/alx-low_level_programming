733c279ed57f07a27d2fe6eddc05e9dfc90254e9
jifel <jiffaelorm@gmail.com>
2022-09-21 08:39:29 -0700

    take if loop out

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,16 +12,10 @@ void puts2(char *str)
 
 	for (i = 0; i < len; i++)
 	{
-		if (i == 0)
-		{
-			_putchar(str[i]);
-		}
-
 		while (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
-
 
 	}
 	_putchar('\n');
