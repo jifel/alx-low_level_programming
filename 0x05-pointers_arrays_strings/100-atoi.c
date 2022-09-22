63ac2f9713f504a2ce1d626c677d946766c170fc
jifel <jiffaelorm@gmail.com>
2022-09-22 06:35:20 -0700

    using atoi to convert a string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _atoi - function that convert a string to an integer
+ * @s: paramter it receives
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int val;
+
+	val = atoi(s);
+
+	return (val);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,3 +12,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
