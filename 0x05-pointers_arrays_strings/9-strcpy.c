ce621f31cc525faa6d1bd37f055ca85bc9bc656e
jifel <jiffaelorm@gmail.com>
2022-09-21 18:10:55 -0700

    using strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies the string pointed to
+ * @dest : paramter it receives
+ * @src: parameter it receives
+ * Return: char *
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	strcpy(src, dest);
+
+	return (dest);
+}
