0714d39ad155cb74c183367ac74cbd3a5c569486
jifel <jiffaelorm@gmail.com>
2022-09-28 09:18:01 -0700

    concactenating two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcat - functions that concatenates two strings
+ * @dest: parameter
+ * @src: parameter
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	strcat(dest, src);
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+#include <string.h>
+int _putchar (int c);
+char *_strcat(char *dest, char *src);
