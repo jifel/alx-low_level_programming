633ba3716957a810320565e4ec5799f3c5a5e03d
jifel <jiffaelorm@gmail.com>
2022-09-28 18:32:56 -0700

    prints a string followed by a newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a newline
+ * @s: paramter
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_putchar('\n');
+}
